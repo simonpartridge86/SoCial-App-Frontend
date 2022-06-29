4cde6615b1a32c2bee7ad290ee7e3af3337bcab2
kunalshukla <kun.shukla@gmail.com>
2022-06-29 15:06:54 +0100

    more testing

--- social/cypress/e2e/spec.cy.js
+++ social/cypress/e2e/spec.cy.js
@@ -3,10 +3,10 @@
 2. test the header is rendering - DONE
 3. form button renders - DONE
 4. Event image renders - DONE
-5. Event details renders - 
-6. Team name renders
-7. Form appears after clicking button
-8. On submitting form, new event should display on the list
+5. Event details renders - DONE
+6. Team name renders - DONE
+7. Form appears after clicking button - DONE
+8. On submitting form, new event should display on the list - 
 9. Form should reset to initial state
 10. Test event image - should render correct image according to event type
 11. Attendance checkbox should update attendance count
@@ -26,9 +26,21 @@ describe('Social page is rendering correctly', () => {
   })
   it('Event Details', () => {
     cy.get(':nth-child(1) > .event-details > .event-type')
-    cy.get(':nth-child(1) > .event-details > .event-descriptions')
+    cy.get(':nth-child(1) > .event-details > .event-description')
     cy.get(':nth-child(1) > .event-details > .event-author')
+    cy.get(':nth-child(1) > .event-details > .date')
+    cy.get(':nth-child(1) > .event-details > .time-start')
+    cy.contains('bootcampers attending')
+    cy.contains('Attending?')
+    cy.contains('Launch Meeting')
+  })
+  it('Footer text', () => {
+    cy.contains('team mishMash')
+  })
+  it('Create event button click', () => {
+    cy.contains ('Create Event').click()
   })
 })
+
 
 
